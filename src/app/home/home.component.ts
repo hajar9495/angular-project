3629abde4dcee367b6db899838d4a1d3a10e76b5
hajar9495 <hajarmennai@grpgformation.onmicrosoft.com>
2024-05-30 11:22:58 +0200

    ajout d'un autre component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HousingLocationComponent],
   templateUrl: './home.component.html',
   // template: ` <p>home works!</p> `,
   // styleUrl: './home.component.css',
 })
 export class HomeComponent {
   city = 'paris';
+  
 }
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  // template: `
+  //   <p>
+  //     housing-location works!
+  //   </p>
+  // `,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {}
