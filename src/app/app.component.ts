9eb9cc13b88c935089db90e2f7c8eef9f4e9b37d
hajar9495 <hajarmennai@grpgformation.onmicrosoft.com>
2024-06-02 14:05:45 +0200

    exemple du site d'angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MaisonComponent } from './maison/maison.component';
 import { TrucComponent } from './truc/truc.component';
+import { TextComponent } from './text/text.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MaisonComponent, TrucComponent],
+  imports: [MaisonComponent, TrucComponent, TextComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
